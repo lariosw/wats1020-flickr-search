9e6058b76776f1ee286c615bd9bf33118b279014
lariosw <lariosw@seattleu.edu>
2016-08-01 00:01:51 -0700

    update flicker changes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,60 @@
 //
 // Allow users to click the images to see a larger version with more information.
 $(document).on('ready', function(){
+    
+    //takes keywords and passes them to flickr for photos from tags
+    function searchItems(whatTheUserTyped) {
+        var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    
+        //call the flicker api and get images that match typed keyword
+        $.getJSON(flickerAPI, {
+            tags: whatTheUserTyped,
+            tagmode: "any",
+            format: "json"
+        })
+        .done(function(data) {
+            //clear out any previous results
+            $('#images').empty();
+      
+            //update search title to include keywords
+            $('h1.search-title').first()[0].innerHTML = "Search for: " + whatTheUserTyped;
+        
+            //iterate through all images in result and add them to dom 
+            $.each( data.items, function( i, item ) {
+              
+                //create new li to hold image information
+                var newListItem = $('<li class="result-img-wrapper">');
+                
+                // add actual image to container
+                var newImgWrapper = $('<div class="image-pic-wrapper">').appendTo(newListItem);
+                var newImg = $('<img class="image-pic">').attr( "src", item.media.m).appendTo(newImgWrapper);
+                
+                //add images details to container
+                var newTitle = $('<p class="image-title">').html('<span class="img-heading">Title:</span> ' + item.title).appendTo(newListItem);
+                var newDate = $('<p class="image-date">').html('<span class="img-heading">Date Taken:</span> ' + item.date_taken).appendTo(newListItem);
+                var newTags = $('<p class="image-tags">').html('<span class="img-heading">Tags:</span> ' + item.tags).appendTo(newListItem);
+                var newLink = $('<a>').attr('href', item.link).text('View on Flickr').appendTo(newListItem);
+
+                //add image info container to image list
+                newListItem.appendTo( "#images" );
+          });
+        });
+    }
+        
+    $('button.search').on('click', function(event) {
+        //prevent default behavior of submit button
+        event.preventDefault();
+     
+        //get the keyword(s) typed by the user
+        var $searchTextInput = $('input.form-control[name="searchText"]');
+        var typedKeyWords = $searchTextInput.val();
+        searchItems(typedKeyWords);
+    });
+});    
+
+
+
+ 
     // Place your code here, inside the document ready handler.
 
     // Create a function called `searchImages()`. This function will handle the
@@ -46,4 +100,4 @@ $(document).on('ready', function(){
 
 
 
-});
+
